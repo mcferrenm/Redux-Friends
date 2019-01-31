016f0e618527978391370fa87337f23659f58f5a
mcferrenm <mcferrenmax@gmail.com>
2019-01-31 16:26:08 -0500

    Modify FriendForm to be controlled by App state

--- friends/src/App.js
+++ friends/src/App.js
@@ -10,7 +10,22 @@ const BASE_URL = "http://localhost:5000";
 
 class App extends Component {
   state = {
-    friends: []
+    friends: [],
+    newFriend: {
+      name: "",
+      email: "",
+      age: ""
+    }
+  };
+
+  handleChange = e => {
+    e.persist();
+    this.setState(prevState => ({
+      newFriend: {
+        ...prevState.newFriend,
+        [e.target.name]: e.target.value
+      }
+    }));
   };
   componentDidMount() {
     axios
@@ -21,7 +36,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <FriendsForm />
+        <FriendsForm
+          newFriend={this.state.newFriend}
+          handleChange={this.handleChange}
+        />
         <FriendsList friends={this.state.friends} />
       </div>
     );
--- friends/src/components/FriendsForm.js
+++ friends/src/components/FriendsForm.js
@@ -3,9 +3,24 @@ import React from "react";
 const FriendsForm = props => {
   return (
     <form className="friends-form" action="">
-      <input type="text" name="name" />
-      <input type="text" name="email" />
-      <input type="number" name="age" />
+      <input
+        type="text"
+        name="name"
+        value={props.newFriend.name}
+        onChange={props.handleChange}
+      />
+      <input
+        type="text"
+        name="email"
+        value={props.newFriend.email}
+        onChange={props.handleChange}
+      />
+      <input
+        type="number"
+        name="age"
+        value={props.newFriend.age}
+        onChange={props.handleChange}
+      />
       <button type="submit">Add Friend</button>
     </form>
   );
